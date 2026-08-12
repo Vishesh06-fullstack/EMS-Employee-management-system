3138e0c8d455d952588c6586855873435ccbf59b
vishesh singh <visheshpratapsingh12@gmail.comgit config --global user.name>
2026-08-12 17:55:37 +0530

    add admin dashboard and employee

---
+++ client/src/component/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='flex h-screen items-center justify-center '>
+        <div className='animate-spin size-8 border-2 border-indigo-600 border-t-transparent rounded-full '>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Loading
--- client/src/component/Sidebar.jsx
+++ client/src/component/Sidebar.jsx
@@ -121,7 +121,8 @@ const Sidebar = () => {
 
       <div className="p-3 border-t border-white/6">
           <button className="flex items-center gap-3 w-full px-3 py-2.5 rounded-md text-[13px] font-medium text-slate-400 hover:text-rose-500 hover:bg-rose-500/8 transition-all duration-150">
-            <LogOutIcon onClick={handleLogout} className="w-[17px] h-[17px] "/>Logout
+            <LogOutIcon onClick={handleLogout} className="w-[17px] h-[17px] "/>
+            <span>Logout</span>
           </button>
       </div>
     </>
